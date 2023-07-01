7d61f2eacbe3a359d31fded38a1517c1f8f49976
Shivam Dhaka <shivamdhaka1200@gmail.com>
2023-07-01 12:14:54 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("File", file);
+
+    const res = await fetch(
+      "https://jablueo323.execute-api.us-east-1.amazonaws.com/file-upload-system-lambda",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        alert("Success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>File Upload System using API Gateway, Lambda and S3</h3>
+      <div>
+        <input type="file" name="file" onChange={handleChange} />
+        <br/>
+        <br/>
+        <button className="button-29" onClick={handleSubmit}>Submit File</button>
+      </div>
+      {isSelected && (
+        <div>
+          <p>Filename : {file.name}</p>
+          <p>Filetype : {file.type}</p>
+          <p>Filesize (bytes) : {file.size}</p>
+        </div>
+      )}
     </div>
   );
 }
